6989aa4b807d923552fdad4fe6917c16ac2b88ff
Giuseppe Ravagnani <giuseppe.ravagnani@gmail.com>
2022-07-05 16:45:24 +0200

    commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/keep-alive")
+def index():
+    return "Alive!"
+
+if __name__ == '__main__':
+   app.run()
