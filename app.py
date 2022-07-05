48d3a75adebd1cb5e1d4fc9065f1b4565ac36cbf
Giuseppe Ravagnani <giuseppe.ravagnani@gmail.com>
2022-07-05 17:01:51 +0200

    commmit

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
-
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
-@app.route("/keep-alive")
-def index():
-    return "Alive!"
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+
+    <img src="http://loremflickr.com/600/400">
+    """.format(time=the_time)
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True, use_reloader=True)
