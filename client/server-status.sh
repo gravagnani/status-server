a6ee00351818217ad0c54b653c04bb235062eb5d
gravagnani <giuseppe.ravagnani@gmail.com>
2022-07-06 23:01:01 +0200

    linux client

---
+++ client/server-status.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+headers="Content-Type: application/json"
+
+url="https://status-server-service.herokuapp.com"
+body="{\"server_code\": \"C1\"}"
+
+wait=300
+
+while true
+do
+    curl --location --request POST "$url" --header "$headers" --data-raw "$body"
+
+    sleep $wait
+done
